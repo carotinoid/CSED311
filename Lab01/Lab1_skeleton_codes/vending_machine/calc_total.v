bcd55b3b388c4f0c067e72106a3ac2582218e6b2
Minseong Gwak <gmspweber@postech.ac.kr>
2025-03-05 21:45:42 +0900

    implement calc_total

--- Lab01/Lab1_skeleton_codes/vending_machine/calc_total.v
+++ Lab01/Lab1_skeleton_codes/vending_machine/calc_total.v
@@ -2,13 +2,11 @@
 
 module calc_total(current_total, i_input_coin, coin_value, next_total);
 
-    input current_total;
+    input [`kTotalBits-1:0] current_total;
     input [`kNumCoins-1:0] i_input_coin;
-    input 
+    input [31:0] coin_value [`kNumCoins-1:0];
+    output [`kTotalBits-1:0] next_total;
 
-    always @(*) begin
-        next_total = current_total;
-    
-    end
+    assign next_total = current_total + coin_value[0] * i_input_coin[0] + coin_value[1] * i_input_value[1] + coin_value[2] * i_input_coin[2];
 
 endmodule
