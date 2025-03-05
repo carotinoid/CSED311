95b7b3665a7744f9a33692e760d5ca85422269e3
Kim Jaehwan <carotinoid1807@gmail.com>
2025-03-05 21:40:57 +0900

    implement change_time.v

--- Lab01/Lab1_skeleton_codes/vending_machine/change_time.v
+++ Lab01/Lab1_skeleton_codes/vending_machine/change_time.v
@@ -1,2 +1,17 @@
 `include "vending_machine_def.v"
 
+module change_state(clk,reset_n,timeset,timeout);
+	input clk;
+	input reset_n;
+    input timeset;
+    output timeout;
+    reg [`kTotalBits : 0] wait_time;
+    assign timeout = (wait_time == 0);
+	
+	always @(posedge clk) begin
+		if (!reset_n) wait_time <= 0;
+		else if (timeset) wait_time <= `kWaitTime;
+        else if (wait_time > 0) wait_time <= wait_time - 1;
+        else wait_time <= 0;
+	end
+endmodule 
