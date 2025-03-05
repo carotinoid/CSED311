c239bd906454e381cfec113517c96d93c5a0e131
Minseong Gwak <gmspweber@postech.ac.kr>
2025-03-05 21:49:00 +0900

    calc_total make better

--- Lab01/Lab1_skeleton_codes/vending_machine/calc_total.v
+++ Lab01/Lab1_skeleton_codes/vending_machine/calc_total.v
@@ -7,6 +7,6 @@ module calc_total(current_total, i_input_coin, coin_value, next_total);
     input [31:0] coin_value [`kNumCoins-1:0];
     output [`kTotalBits-1:0] next_total;
 
-    assign next_total = current_total + coin_value[0] * i_input_coin[0] + coin_value[1] * i_input_value[1] + coin_value[2] * i_input_coin[2];
+    assign next_total = current_total + coin_value[0] * i_input_coin[0] + coin_value[1] * i_input_coin[1] + coin_value[2] * i_input_coin[2];
 
 endmodule
