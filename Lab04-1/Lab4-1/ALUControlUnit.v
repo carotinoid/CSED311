6ec6a65693ed073bce6aa89f630fb97f65741871
Minseong Gwak <gmspweber@postech.ac.kr>
2025-04-22 19:58:51 +0900

    Lab4 - Legacy inherited

---
+++ Lab04-1/Lab4-1/ALUControlUnit.v
@@ -0,0 +1,55 @@
+`include "alu_op.v"
+`include "opcodes.v"
+
+module ALUControlUnit(input Instr30,
+                     input [2:0] funct3,
+                     input [6:0] opcode,
+                     output reg [7:0] alu_op);
+
+    wire [6:0] funct7;
+    assign funct7 = {1'b0, Instr30, 5'b00000};
+
+    always @(*) begin
+        case(opcode)
+            `ARITHMETIC : begin
+                case(funct3) 
+                    `FUNCT3_ADD : begin // with `FUNCT7_SUB
+                        if(funct7 == `FUNCT7_SUB) alu_op = `SUB;
+                        else alu_op = `ADD;
+                    end
+                    `FUNCT3_SLL : alu_op = `SLL;    
+                    `FUNCT3_XOR : alu_op = `XOR;    
+                    `FUNCT3_OR : alu_op = `OR;    
+                    `FUNCT3_AND : alu_op = `AND;    
+                    `FUNCT3_SRL : alu_op = `SRL;  
+                    default: alu_op = `ADD;  
+                endcase
+            end
+            `ARITHMETIC_IMM : begin
+                case(funct3)
+                    `FUNCT3_ADD : alu_op = `ADD;
+                    `FUNCT3_SLL : alu_op = `SLL;
+                    `FUNCT3_XOR : alu_op = `XOR;
+                    `FUNCT3_OR : alu_op = `OR;
+                    `FUNCT3_AND : alu_op = `AND;
+                    `FUNCT3_SRL : alu_op = `SRL;
+                    default: alu_op = `ADD;
+                endcase
+            end
+            `LOAD, `STORE, `JALR : begin
+                alu_op = `ADD;
+            end
+            `BRANCH : begin
+                case(funct3)
+                    `FUNCT3_BEQ : alu_op = `BEQ;
+                    `FUNCT3_BNE : alu_op = `BNE;
+                    `FUNCT3_BLT : alu_op = `BLT;
+                    `FUNCT3_BGE : alu_op = `BGE;
+                    default: alu_op = `BEQ;
+                endcase
+            end
+            default: alu_op = `ADD;
+        endcase
+    end
+    
+endmodule
---
+++ Lab04-1/Lab4-1/ControlUnit.v
@@ -0,0 +1,26 @@
+`include "opcodes.v"
+
+module ControlUnit (input [6:0] Instr,
+                     output JAL,
+                     output JALR,
+                     output Branch,
+                     output MemRead,
+                     output MemtoReg,
+                     output MemWrite,
+                     output ALUSrc,
+                     output RegWrite,
+                     output PCtoReg,
+                     output is_ecall);
+
+assign JAL = (Instr == `JAL);
+assign JALR = (Instr == `JALR);
+assign Branch = (Instr == `BRANCH);
+assign MemRead = (Instr == `LOAD);
+assign MemtoReg = (Instr == `LOAD);
+assign MemWrite = (Instr == `STORE);
+assign ALUSrc = (Instr != `ARITHMETIC) && (Instr != `BRANCH);
+assign RegWrite = (Instr != `STORE) && (Instr != `BRANCH);
+assign PCtoReg = (Instr == `JAL) || (Instr == `JALR);
+assign is_ecall = (Instr == `ECALL);
+
+endmodule
